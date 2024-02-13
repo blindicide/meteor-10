adc2c0a6664acef6aec5f3a8c11e997f759a02ae
blindicide-w3nd1g0 <victrola.766@gmail.com>
2024-02-13 11:26:00 +0300

    flushfiles

---
+++ flushfiles.py
@@ -0,0 +1,4 @@
+filegpt = open('t1-gpt35.txt', 'w', encoding='utf-8')
+filellama = open('t1-llama13b.txt', 'w', encoding='utf-8')
+filemistral = open('t1-mistral.txt', 'w', encoding='utf-8')
+fileyandex = open('t1-ygpt.txt', 'w', encoding='utf-8')
